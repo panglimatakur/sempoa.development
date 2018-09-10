153536a337901f44dcd1bd6b6b4f5cede9e606fc
panglimatakur <thetakur@gmail.com>
2018-09-10 13:42:46 +0700

    Test Index Main 4

--- zendback/templates/admin-v2.1/index.php
+++ zendback/templates/admin-v2.1/index.php
@@ -324,7 +324,15 @@
     <script src="<?php echo $web_btpl_dir; ?>js/plugins/peity/jquery.peity.min.js"></script>
     <script src="<?php echo $web_btpl_dir; ?>js/demo/peity-demo.js"></script>
 
-    
+    <!-- SlimScroll -->
+    <script src="<?php echo $web_btpl_dir; ?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="<?php echo $web_btpl_dir; ?>js/inspinia.js"></script>
+    <script src="<?php echo $web_btpl_dir; ?>js/plugins/pace/pace.min.js"></script>
+
+    <!-- jQuery UI -->
+    <script src="<?php echo $web_btpl_dir; ?>js/plugins/jquery-ui/jquery-ui.min.js"></script>
    	<?php
         if(is_file($page_dir."/js/js.js"))	{  ?>
             <script language='javascript' src='<?php echo $page_dir; ?>/js/js.js'></script> 
