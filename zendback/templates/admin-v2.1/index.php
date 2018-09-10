4276f14866fddc2e54651629825449986f693c26
panglimatakur <thetakur@gmail.com>
2018-09-10 15:42:26 +0700

    Test Index Main 14

--- zendback/templates/admin-v2.1/index.php
+++ zendback/templates/admin-v2.1/index.php
@@ -297,7 +297,7 @@
                 
             <div class="wrapper wrapper-content animated fadeInRight">
                 <div class="row">
-					<?php if(!empty($page)){ include $call->inc("zendback/pages","index.php"); } ?>
+					<?php if(!empty($page)){  } ?>
                 </div>  
             </div>
 
